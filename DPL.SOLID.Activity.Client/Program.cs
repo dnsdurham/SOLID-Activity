f9d6897ed20d4a6cf092508b8acddff0588b6261
Doug Durham <ddurham@nebraskaglobal.com>
2017-01-01 08:29:23 -0600

    Creating activity projects

---
+++ DPL.SOLID.Activity.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPL.SOLID.Activity.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
